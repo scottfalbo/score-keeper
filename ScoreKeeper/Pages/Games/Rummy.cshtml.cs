c7dbf8caef68750fcdf501af84e9448e8fbc71ac
Scott Falbo <scottfalboart@gmail.com>
2021-06-15 23:01:08 -0700

    Basic scaffolding

---
+++ ScoreKeeper/Pages/Games/Rummy.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScoreKeeper.Pages.Games
+{
+    public class RummyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
