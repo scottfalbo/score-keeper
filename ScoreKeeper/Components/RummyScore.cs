2168d1238fb094f93415ed386fff51657eb1e46c
Scott Falbo <scottfalboart@gmail.com>
2021-06-15 23:11:02 -0700

    added rummy score view component

---
+++ ScoreKeeper/Components/RummyScore.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScoreKeeper.Components
+{
+    [ViewComponent]
+    public class RummyScore : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+
+    }
+}
